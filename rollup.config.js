d91c22086e77a08b53e7def66d284720bb08d817
NuclearRedeye <j.summerfield@elifesciences.org>
2020-03-24 16:50:23 +0000

    feat: initial commit of a buggy editor

---
+++ rollup.config.js
@@ -0,0 +1,37 @@
+import { default as copy } from 'rollup-plugin-copy';
+import resolve from 'rollup-plugin-node-resolve';
+import commonJS from 'rollup-plugin-commonjs';
+
+module.exports = {
+  input: './src/index.js',
+  output: [
+    {
+      file: 'dist/index.js',
+      format: 'cjs',
+      sourcemap: true
+    },
+    {
+      file: 'dist/index.es.js',
+      format: 'es',
+      sourcemap: true
+    }
+  ],
+  plugins: [
+    resolve(),
+    commonJS({
+      include: 'node_modules/**'
+    }),
+    copy({
+      targets: [
+        {
+          src: 'src/index.html',
+          dest: 'dist/'
+        },
+        {
+          src: './node_modules/prosemirror-example-setup/style/style.css',
+          dest: 'dist/'
+        }
+      ]
+    })
+  ]
+};
