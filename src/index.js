ba8ca52423208978acab5cad80e5b6d400231884
NuclearRedeye <j.summerfield@elifesciences.org>
2020-03-26 11:43:10 +0000

    chore: setup for custom schema

--- src/index.js
+++ src/index.js
@@ -1,10 +1,10 @@
 import { EditorState } from 'prosemirror-state';
 import { EditorView } from 'prosemirror-view';
 import { Schema, DOMParser } from 'prosemirror-model';
-import { schema } from 'prosemirror-schema-basic';
+import { schema } from './schema-basic.js';
 import { addListNodes } from 'prosemirror-schema-list';
 import { exampleSetup } from 'prosemirror-example-setup';
-import { defaultExampleDOM } from './content.js';
+import { defaultExampleDOM, boxedTextExampleDOM } from './content.js';
 
 // Mix the nodes from prosemirror-schema-list into the basic schema to
 // create a schema with list support.
