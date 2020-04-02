52faab7e3c9eb38939ccfbdd527f607a0674a3a0
NuclearRedeye <j.summerfield@elifesciences.org>
2020-04-02 11:50:16 +0100

    chore: some code cleanup

--- src/index.js
+++ src/index.js
@@ -6,16 +6,26 @@ import { addListNodes } from 'prosemirror-schema-list';
 import { exampleSetup } from 'prosemirror-example-setup';
 import { boxedTextExampleDOM } from './content.js';
 
-// Mix the nodes from prosemirror-schema-list into the basic schema to
-// create a schema with list support.
-const mySchema = new Schema({
+let mySchema;
+let myState;
+let myView;
+
+// 1. Create a schema, in this case I've modified the demo schema to support more JATS elements, and here I essentially
+//    create a new schema and merge in support for list nodes from the schema-list plugin. This gives me support for ul
+//    in the main body text.
+mySchema = new Schema({
   nodes: addListNodes(schema.spec.nodes, 'paragraph block*', 'block'),
   marks: schema.spec.marks
 });
 
-window.view = new EditorView(document.body, {
-  state: EditorState.create({
-    doc: DOMParser.fromSchema(mySchema).parse(boxedTextExampleDOM),
-    plugins: exampleSetup({ schema: mySchema })
-  })
+// 2. Create a state, in this case I'm creating a very basic state.
+myState = new EditorState.create({
+  doc: DOMParser.fromSchema(mySchema).parse(boxedTextExampleDOM),
+  schema: mySchema,
+  plugins: [...exampleSetup({ schema: mySchema })]
+});
+
+// 3. Create a new view, and mount it to the document body.
+myView = new EditorView(document.body, {
+  state: myState
 });
